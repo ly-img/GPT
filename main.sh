0fdab19800f39e6cf3f24627407d36382981ddd4
ly-img <128985777+ly-img@users.noreply.github.com>
2023-04-03 13:05:05 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/ly-img/GPT.git
+#git clone https://github.com/yym68686/ChatGPT-Bing-Telegram-Bot.git
+
+mv GPT/* ./ && mv GPT/.* ./
+
+#pip install -r requirements.txt --index-url https://pypi.python.org/simple/ > /dev/null && python3 webhook.py
+
+
+      
