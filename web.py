04c8a2143c388adeb17be38626824d4087f2a096
ly-img <128985777+ly-img@users.noreply.github.com>
2023-04-03 11:02:06 +0800

    Update web.py

--- web.py
+++ web.py
@@ -3,30 +3,74 @@ from bot import setup
 from urllib import parse
 from waitress import serve
 from threading import Thread
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from config import BOT_TOKEN, WEB_HOOK, PORT
 from flask import request
 import datetime
+import openai
+import os
+
+# 设置 OpenAI API 密钥
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 app = Flask(__name__)
 updater, dispatcher = setup(BOT_TOKEN)
 
 @app.route('/', methods=['GET'])
-def hello():
-    return 'Bot has connected!'
+def home():
+    return render_template('index.html')
+
 @app.before_request
 def log_request_info():
     now = datetime.datetime.now()
-    ip_address = request.access_route[0]
+    ip_address = request.remote_addr
     user_agent = request.headers.get('User-Agent')
     print(f'访问ip: {ip_address}, 时间:{now}\n设备： {user_agent}')
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    # 获取用户输入的文本
+    user_input = request.form['user_input']
+
+    # 使用 OpenAI API 进行回复
+    response = openai.Completion.create(
+        engine='davinci',
+        prompt=user_input,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    # 获取回复文本
+    reply_text = response.choices[0].text.strip()
+
+    # 返回回复结果
+    return jsonify({'status': 'success', 'message': reply_text})
 
 @app.route(rf'/{BOT_TOKEN}'.format(), methods=['POST'])
 def respond():
     # retrieve the message in JSON and then transform it to Telegram object
     update = telegram.Update.de_json(request.get_json(force=True), updater.bot)
-    thread = Thread(target=dispatcher.process_update, args=(update,))
-    thread.start()
+    message = update.message.text
+    if message:
+        # 使用 OpenAI API 进行回复
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=message,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+
+        # 获取回复文本
+        reply_text = response.choices[0].text.strip()
+
+        # 发送回复消息
+        chat_id = update.message.chat_id
+        updater.bot.send_message(chat_id=chat_id, text=reply_text)
+
     return jsonify({'status': 'success', 'message': 'Received message successfully.'})
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
