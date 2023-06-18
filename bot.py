d8c92158e7b370b79b1051a8b9ee2a7cf304fafc
ly-img <128985777+ly-img@users.noreply.github.com>
2023-06-19 00:17:50 +0800

    update README

--- bot.py
+++ bot.py
@@ -1,147 +1,68 @@
-import re
+import logging
+from md2tgmd import escape
+from runasync import run_async
+from telegram import BotCommand
+from telegram.ext import CommandHandler, MessageHandler, ApplicationBuilder, filters
+from AI import AIBot
+ai_bot = AIBot()
 
-def escapeshape(text):
-    poslist = [0]
-    strlist = []
-    originstr = []
-    regex = r"(^#+\s.+?$)|```[\D\d\s]+?```"
-    matches = re.finditer(regex, text, re.MULTILINE)
-    for match in matches:
-        start = match.start(1)
-        end = match.end(1)
-        if match.group(1) != None:
-            poslist += [start, end]
-            strlist.append('▎*' + text[start:end].split()[1] + '*')
-    poslist.append(len(text))
-    for i in range(0, len(poslist), 2):
-        j, k = poslist[i:i+2]
-        originstr.append(text[j:k])
-    if len(strlist) < len(originstr):
-        strlist.append('')
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
+
+# In all other places characters
+# _ * [ ] ( ) ~ ` > # + - = | { } . !
+# must be escaped with the preceding character '\'.
+async def start(update, context): # 当用户输入/start时，返回文本
+    user = update.effective_user
+    message = (
+        "我是人见人爱的 ChatGPT AI机器人 \n\n"
+        "欢迎访问 https://github.com/ly-img/GPT 查看源码\n\n"
+    )
+    await update.message.reply_html(rf"Hi {user.mention_html()} ! I am an Assistant, a large language model trained by OpenAI. I will do my best to help answer your questions.",)
+    await update.message.reply_text(escape(message), parse_mode='MarkdownV2')
+
+async def en2zhtranslator(update, context):
+    if len(context.args) > 0:
+        message = ' '.join(context.args)
+        print("\033[32m")
+        print("en2zh", message)
+        print("\033[0m")
+        ai_bot.en2zhtranslator(message, update, context)
     else:
-        originstr.append('')
-    new_list = [item for pair in zip(originstr, strlist) for item in pair]
-    return ''.join(new_list)
+        message = await context.bot.send_message(
+            chat_id=update.message.chat_id,
+            text="请在命令后面放入要翻译的文本。",
+            parse_mode='MarkdownV2',
+            reply_to_message_id=update.message.message_id,
+        )
 
-def escape(text):
-    # In all other places characters
-    # _ * [ ] ( ) ~ ` > # + - = | { } . !
-    # must be escaped with the preceding character '\'.
-    # text = re.sub(r"\\n", r"\\\\n", text)
-    text = re.sub(r"\\", r"\\\\", text)
-    text = re.sub(r"_", '\_', text)
-    text = re.sub(r"\*{2}(.*?)\*{2}", '@@@\\1@@@', text)
-    text = re.sub(r"\n\*\s", '\n\n• ', text)
-    text = re.sub(r"\*", '\*', text)
-    text = re.sub(r"\@{3}(.*?)\@{3}", '*\\1*', text)
-    text = re.sub(r"\!?\[(.*?)\]\((.*?)\)", '@@@\\1@@@^^^\\2^^^', text)
-    text = re.sub(r"\[", '\[', text)
-    text = re.sub(r"\]", '\]', text)
-    text = re.sub(r"\(", '\(', text)
-    text = re.sub(r"\)", '\)', text)
-    text = re.sub(r"\@{3}(.*?)\@{3}\^{3}(.*?)\^{3}", '[\\1](\\2)', text)
-    text = re.sub(r"~", '\~', text)
-    text = re.sub(r">", '\>', text)
-    text = escapeshape(text)
-    text = re.sub(r"#", '\#', text)
-    text = re.sub(r"`(.*?)\+(.*?)`", '`\\1@@@\\2`', text)
-    text = re.sub(r"\+", '\+', text)
-    text = re.sub(r"\@{3}", '+', text)
-    text = re.sub(r"\n(\s*)-\s", '\n\n\\1• ', text)
-    text = re.sub(r"`(.*?)-(.*?)`", '`\\1@@@\\2`', text)
-    text = re.sub(r"\-", '\-', text)
-    text = re.sub(r"\@{3}", '-', text)
-    text = re.sub(r"=", '\=', text)
-    text = re.sub(r"\|", '\|', text)
-    text = re.sub(r"{", '\{', text)
-    text = re.sub(r"}", '\}', text)
-    text = re.sub(r"\.", '\.', text)
-    text = re.sub(r"!", '\!', text)
-    return text
+async def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+    await context.bot.send_message(chat_id=update.message.chat_id, text="出错啦！请重试。", parse_mode='MarkdownV2')
 
-def bingescape(text):
-    text = re.sub(r"\\\\", '@@@', text)
-    text = re.sub(r"\\", r"\\\\", text)
-    text = re.sub(r"\@{3}", r"\\\\", text)
-    text = re.sub(r"_", '\_', text)
-    text = re.sub(r"\*{2}(.*?)\*{2}", '@@@\\1@@@', text)
-    text = re.sub(r"\n\*\s", '\n\n• ', text)
-    text = re.sub(r"\*", '\*', text)
-    text = re.sub(r"\@{3}(.*?)\@{3}", '*\\1*', text)
-    text = re.sub(r"\!?\[(.*?)\]\((.*?)\)", '@@@\\1@@@^^^\\2^^^', text)
-    text = re.sub(r"\[", '\[', text)
-    text = re.sub(r"\]", '\]', text)
-    text = re.sub(r"\(", '\(', text)
-    text = re.sub(r"\)", '\)', text)
-    text = re.sub(r"\@{3}(.*?)\@{3}\^{3}(.*?)\^{3}", '[\\1](\\2)', text)
-    text = re.sub(r"~", '\~', text)
-    text = re.sub(r">", '\>', text)
-    text = escapeshape(text)
-    text = re.sub(r"#", '\#', text)
-    text = re.sub(r"`(.*?)\+(.*?)`", '`\\1@@@\\2`', text)
-    text = re.sub(r"\+", '\+', text)
-    text = re.sub(r"\@{3}", '+', text)
-    text = re.sub(r"\n(\s*)-\s", '\n\n\\1• ', text)
-    text = re.sub(r"`(.*?)-(.*?)`", '`\\1@@@\\2`', text)
-    text = re.sub(r"\-", '\-', text)
-    text = re.sub(r"\@{3}", '-', text)
-    text = re.sub(r"=", '\=', text)
-    text = re.sub(r"\|", '\|', text)
-    text = re.sub(r"{", '\{', text)
-    text = re.sub(r"}", '\}', text)
-    text = re.sub(r"\.", '\.', text)
-    text = re.sub(r"!", '\!', text)
-    return text
+async def unknown(update, context): # 当用户输入未知命令时，返回文本
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
-text = r'''
-# title
+def setup(token):
+    application = ApplicationBuilder().read_timeout(10).connection_pool_size(50000).pool_timeout(1200.0).token(token).build()
+    
+    run_async(application.bot.set_my_commands([
+        BotCommand('start', 'Start the bot'),
+        BotCommand('reset', 'Reset the bot'),
+        BotCommand('en2zh', 'translate English to Chinese'),
+        BotCommand("creative_bing", "get a creative new Bing"),
+        BotCommand("balanced_bing", "get a balanced new Bing"),
+        BotCommand("precise_bing", "get a precise new Bing"),
+    ]))
 
-**bold**
-```
-# comment
-print(qwer) # ferfe
-ni1
-```
-# bn
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("reset", ai_bot.reset_chat))
+    application.add_handler(CommandHandler("en2zh", ai_bot.en2zhtranslator))
+    application.add_handler(CommandHandler("creative_bing", ai_bot.creative_bing))
+    application.add_handler(CommandHandler("balanced_bing", ai_bot.balanced_bing))
+    application.add_handler(CommandHandler("precise_bing", ai_bot.precise_bing))
+    application.add_handler(MessageHandler(filters.TEXT, ai_bot.getResult))
+    application.add_handler(MessageHandler(filters.COMMAND, unknown))
+    application.add_error_handler(error)
 
-# b
-
-# Header
-## Subheader
-
-[1.0.0](http://version.com)
-![1.0.0](http://version.com)
-
-- item 1 -
-    - item 1 -
-    - item 1 -
-* item 2 #
-* item 3 ~
-
-sudo apt install mesa-utils # 安装
-
-```python
-print("1.1\n")_
-\subsubsection{1.1}
-```
-\subsubsection{1.1}
-
-And simple text `with-ten`  `with+ten` + some - **symbols**. # `with-ten`里面的`-`不会被转义
-
-
-```
-print("Hello, World!")
-```
-
-Cxy = abs (Pxy)**2/ (Pxx*Pyy)
-
-\\begin {equation}
-\pi
-\\end {equation*}
-'''
-
-if __name__ == '__main__':
-    import os
-    os.system('clear')
-    text = escape(text)
-    print(text)
+    return application
